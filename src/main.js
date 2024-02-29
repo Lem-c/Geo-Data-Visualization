575453fb006eacb7a0171d949bf1f68d754cd298
Mr.Lem <cccccyh6048@gmail.com>
2024-02-29 09:51:14 +0000

    Norm update

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 // You can change this import to `import router from './starterRouter'` to quickly start development from a blank layout.
-import router from './router';
+import router from './router.js';
 
 import NowUiKit from './plugins/now-ui-kit';
 
