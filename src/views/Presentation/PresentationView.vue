a8fa5f033d9da9fadd2af985088f35e3afc191d5
Juan Lamilla <juan.e.lamilla@gmail.com>
2024-05-14 20:08:57 +0100

    Update text

--- src/views/Presentation/PresentationView.vue
+++ src/views/Presentation/PresentationView.vue
@@ -39,6 +39,10 @@ onUnmounted(() => {
 </script>
 
 <template>
+
+  
+
+
   <div class="container-fluid position-sticky z-index-sticky top-0">
     <div class="row">
       <div class="col-12">
@@ -46,24 +50,28 @@ onUnmounted(() => {
       </div>
     </div>
   </div>
+  
   <Header>
     <div
       class="page-header min-vh-75"
       :style="`background-image: url(${vueMkHeader})`"
       loading="lazy"
     >
-      <div class="container">
+    <div class="container">
         <div class="row">
           <div class="col-lg-7 text-center mx-auto position-relative">
             <h1
               class="text-black pt-3 me-2"
               :style="{ display: 'inline-block', marginTop: '-200px' }"
             >
-              UK's Economic Horizons
+            — UK's Economic Horizons —
             </h1>
             <p class="lead text-dark px-5 mt-3" :style="{ fontWeight: '500' }">
               Interactive dashboards navigating the trajectories of trade and economic resilience at 3 geographic levels
             </p>
+            <p>‏</p>
+            <p>‏</p>
+            <p>‏</p>
           </div>
         </div>
       </div>
@@ -75,12 +83,36 @@ onUnmounted(() => {
     <div class="container-fluid">
       <div class="row justify-content-center">
         <div class="col-lg-8">
-          <h2 class="text-center mb-4">What this website does</h2>
-          <p class="lead text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p></p>
+          <h2 class="text-center mb-4">Economic Health: Navigating Sustainable Urban Growth</h2>
+          <p class="lead text-center">Economic health can have a profound impact on urban development, infrastructure, and the well-being of the population. Strong economic health ensures funding for public services such as health care, education, and transportation, which are essential components of urban life. In addition, economic strategy affects likelihood of private investment in urban areas, determining employment opportunities and influencing business patterns, all while affecting everything from housing and business development to cultural and recreational facilities. Understanding and planning for economic fluctuations is therefore essential for effectively managing sustainable urban growth.</p>
+          
+          <h2 class="text-center mb-4">Shaping the Future City</h2>
+          <p class="lead text-center">Economic insight is an integral part of the concept of the future city, which aims to be smarter, more sustainable, and more resilient. A city's ability to understand and adapt to economic changes and withstand economic shocks can help determine its long-term success and sustainability.</p>
+          
+          <h2 class="text-center mb-4">The UK: A Global Economic Engine</h2>
+          <p class="lead text-center">The UK is a key node in the global trading network, with the UK’s air, land, and sea ports being among the busiest in the world, facilitating a large number of imports and exports. The UK has a complex and diverse economic landscape, including global financial centres such as London, industrial areas in the Midlands, and emerging tech cities such as Manchester and Bristol. This diversity provides a wide range of data and scenarios for urban planning, from managing booming urban centers to revitalizing post-industrial towns.</p>
+          
+          <h2 class="text-center mb-4">Understanding Economic Recoveries</h2>
+          <p class="lead text-center">The COVID-19 pandemic has had an impact on the UK's economy, and London's performance in economic recovery stands out globally, demonstrating the resilience of global cities and providing a framework for economic recovery that can be used in the future.</p>
         </div>
       </div>
     </div>
   </div>
+
+  <div class="card card-body blur shadow-blur mx-3 mx-md-2 mt-5">
+    <div class="container-fluid">
+      <div class="row justify-content-center">
+        <div class="col-lg-8">
+          <p></p>
+          <h2 class="text-center mb-4">Using this Website</h2>
+          <p class="lead text-center">This website utilizes a series of dashboards, visual elements, and interactive maps in order for you to detect patterns, analyze trends, and uncover the economic well-being of the UK over <b>time</b>.</p>
+          <p class="lead text-center">This site is divided into 3 sections, representing <b>3 geographic levels</b>. This allows us to best understand the economic inbalances across the UK's diverse economic landscapes. You'll begin with an <b>international</b> outlook at the UK's trade relations with the world, continued by a breakdown of the impacts on a <b>subnational</b> level. You'll quickly notice how a certain region (London) dominates the economic makeup of the country, thus you'll finish with a <b>local</b> view on the specifics.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
 
   <div class="card card-body blur shadow-blur mx-3 mx-md-2 mt-5">
 
@@ -263,6 +295,74 @@ onUnmounted(() => {
         </div>
       </div>
     </div>
+
+    <!-- </div>
+
+  <div class="card card-body blur shadow-blur mx-3 mx-md-2 mt-5"> -->
+    <p></p>
+    <p></p>
+    <h2 class="text-center mb-4">Technologies Used</h2>
+    <p class="lead text-center"></p>
+  
+    
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-4">
+          <FilledInfoCard
+            class="px-lg-1 mt-lg-0 mt-4 p-4"
+            :icon="{ component: 'receipt_long', color: 'dark' }"
+            title="D3.js"
+            description="D3.js was alongside the various other tools to create a cohesive interactive environment (international/subnational/local)."
+            :action="{
+              route:
+                'https://d3js.org',
+              label: { text: 'd3js.org', color: 'info' }
+            }"
+          />
+        </div>
+        <div class="col-lg-4">
+          <FilledInfoCard
+            class="px-lg-1 mt-lg-0 mt-4 p-4"
+            :icon="{ component: 'receipt_long', color: 'dark' }"
+            title="DataMaps"
+            description="D3 sublibrary used to create orthographic map projection (international)."
+            :action="{
+              route:
+                'https://datamaps.github.io',
+              label: { text: 'datamaps.github.io', color: 'info' }
+            }"
+          />
+        </div>
+        <div class="col-lg-4">
+          <FilledInfoCard
+            class="px-lg-1 mt-lg-0 mt-4 p-4"
+            :icon="{ component: 'receipt_long', color: 'dark' }"
+            title="Tableau"
+            description="Tableau was used to create various dashboards (international/subnational/local)."
+            :action="{
+              route:
+                'https://www.tableau.com/en-gb',
+              label: { text: 'tableau.com', color: 'info' }
+            }"
+          />
+        </div>
+        <div class="col-lg-4">
+          <FilledInfoCard
+            class="px-lg-1 mt-lg-0 mt-4 p-4"
+            :icon="{ component: 'receipt_long', color: 'dark' }"
+            title="Mapbox"
+            description="Mapbox was used to create the various interactive maps (subnational/local)."
+            :action="{
+              route:
+                'https://www.mapbox.com',
+              label: { text: 'mapbox.com', color: 'info' }
+            }"
+          />
+        </div>
+      </div>
+    </div>
+
+    <p></p>
 
   </div>
 
